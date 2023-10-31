62b223ffa62a082420631debc135e66b88069b9b
nijo6357 <nijo6357@colorado.edu>
2023-10-31 10:57:13 -0600

    ica10

---
+++ ica10.js
@@ -0,0 +1,24 @@
+const button = document.querySelector("button");
+const img = document.querySelector("img");
+
+const div = document.getElementById("hover")
+div.addEventListener("mouseover",hoverYes) // mouseenter mouse exit
+// look up js event listeners
+
+button.addEventListener('click',changePic)
+
+function changePic() {
+    console.log("e")
+    img.src = "DAFOE2.jpeg"
+}
+
+function hoverYes() {
+    console.log("eee")
+    div.innerText = "WHY DID YOU HOVER???????"
+}
+
+
+
+
+
+
