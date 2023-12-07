2e1545f036c8ec057759232a52c403e75d4cf35a
nijo6357 <nijo6357@colorado.edu>
2023-12-06 21:38:04 -0700

    js-final

---
+++ js.final.js
@@ -0,0 +1,9 @@
+function del() {
+    var tbInput = document.getElementById("tbInput");
+    tbInput.value = tbInput.value.substr(0, tbInput.value.length - 1);
+}
+
+ /* function type () {
+    var buttonInput = document.getElementsById ("tbInput");
+    buttonInput.value = 
+} */
